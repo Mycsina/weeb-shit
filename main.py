4eaab8dc6ab5939222817a143398ef06c3b639c8
Mycsina <andremacardoso@gmail.com>
2020-03-27 22:41:18 +0000

    Primite argument handling for quality settings has been done Code explanations for settings have been added

--- main.py
+++ main.py
@@ -9,16 +9,19 @@ from math import ceil
 
 from bs4 import BeautifulSoup, SoupStrainer
 
+import click
+
 import requests as r
 
 time_format = f"%d-%m-%Y %H:%M:%S"
 IND = 0
 
+# Enable if you want to try to automatize this and wish to save logs
 logging.basicConfig(
-    filename=f"/logs/main-{datetime.now().strftime(time_format)}",
-    format=f"%(asctime) %(message)",
-    datefmt=f"%d/%m/%Y %I:%M%S",
-    level=logging.DEBUG,
+    # filename=f"/logs/main-{datetime.now().strftime(time_format)}",
+    # format=f"%(asctime) %(message)",
+    # datefmt=f"%d/%m/%Y %I:%M%S",
+    level=logging.INFO,
 )
 
 
@@ -27,7 +30,7 @@ class HSubsAPI:
         """
         Build a HSubsAPI object with the requestlink attribute which points to the HorribleSubs API.
 
-        It has the how_many_pages() method which gives back the amount of API pages there are.
+        It has the how_many_pages() method which gives back the amount of API pages there are. The how_many_eps() is clear enough.
         Show_id is their internal ID, page is the page number you want to access.
         """
         self.requestlink = f"https://horriblesubs.info/api.php?method=getshows&type=show&showid={show_id}&nextid={page}"
@@ -80,14 +83,15 @@ def get_episodes(url, quality="3"):
     for magnet in entries:
         IND += 1
         logging.debug(magnet)
+        # Saving location
         with open(f"/home/mycsina/Desktop/{IND}.magnet", "w+") as f:
             f.write(magnet)
 
 
 def series_logger(show_id, url):
-    """Given the show_id and it's url it makes a basic log ({date} - {anime} - {ep})."""
+    """Given the show_id and it's url, it makes a basic log ({date} - {anime} - {ep})."""
     series_name = re.search("/shows/(.*)/", url)
-    print(re.sub("-", " ", series_name[1]).title())
+    logging.info(f'{re.sub("-", " ", series_name[1]).title()} is done')
     cleaned_name = re.sub("-", " ", series_name[1]).title()
     show_ep = HSubsAPI(show_id, 0).how_many_eps()
     with open("series-log.txt", "a+") as f:
@@ -95,17 +99,46 @@ def series_logger(show_id, url):
         f.write(entry)
 
 
-def tasker():
+@click.command()
+@click.option(
+    "-Q",
+    "--quality",
+    default="3",
+    type=str,
+    show_default=True,
+    help="1 is 480p, 2 is 720p and 3 is 1080p. Applies to all entries",
+)
+@click.option(
+    "-q",
+    "--individual-quality",
+    nargs=2,
+    default=("3", "0"),
+    type=str,
+    show_default=True,
+    help="First entry is for the quality (1: 480p, 2: 720p, 3: 1080p). Second is for which entry should be different (1 -> 1st entry, 3 -> 3rd entry",
+)
+def tasker(quality, individual_quality):
     """Tasker program that loads HS links from a JSON file."""
     with open("list.json", "r") as f:
         lista = json.load(f)
+        individual_quality = list(individual_quality)
     for entry in lista:
-        logging.debug(entry)
-        get_episodes(entry)
-    time.sleep(10)
+        if int(individual_quality[1]) == 1:
+            individual_quality[1] = int(individual_quality[1]) - 1
+            logging.info(f"Working on: {entry}")
+            logging.info(f"Working with custom quality type {individual_quality[0]}")
+            get_episodes(entry, individual_quality[0])
+        else:
+            individual_quality[1] = int(individual_quality[1]) - 1
+            logging.info(f"Working on: {entry}")
+            get_episodes(entry, quality)
+    # Make sure that the torrent client has time to add all of the .magnet files
+    time.sleep(5)
+    # Location to delete any leftovers from
     dir_name = "/home/mycsina/Desktop/"
     dir_list = os.listdir(dir_name)
     for item in dir_list:
+        # This was made this way because Deluge was tagging the .magnet files as .magnet.invalid even when told to delete them
         if item.endswith(".invalid"):
             os.remove(os.path.join(dir_name, item))
 
